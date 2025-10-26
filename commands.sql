3c807b8b05bf353fdfa228286d1dc6bb254c3c53
Ybala maloth <balasainik3@gmail.com>
2025-10-27 00:21:45 +0530

    first commit

---
+++ commands.sql
@@ -0,0 +1,29 @@
+                      -- DDL (data definition language)
+-- 1. create 
+-- to create database and tables 
+-- 2. alter 
+-- to change name of an column
+-- 3. drop
+-- to delete complete table , used drop command
+-- 4. truncate 
+-- to clean the data from the table , use truncate command 
+ 
+
+                         -- DmL ( Data Manipulation Language )
+   --1. insert
+   -- to insert the values into table
+
+   --2.update 
+   -- to update student details
+
+   --3.delete 
+   -- to delete the a specific value of column or row
+
+                                  --DCL (Data control language )
+ --1.grant
+ -- to gave axies of an table , to check details of an student , like head of clg or school
+
+ --2.revoke 
+ -- to remove the axies that granted by the grant command , beacuse clg or school head is changed 
+ 
+                               
